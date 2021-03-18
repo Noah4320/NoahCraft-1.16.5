e074d257efd9a6f0a6f5a02d66d395cbf18de3de
Noah4320 <Noah_12320@hotmail.com>
2021-03-18 18:44:51 -0400

    Fully implemented gui screen
    Re-added gui texture and cleaned up screen.

--- src/main/java/com/Noah4320/NoahCraft/client/render/screen/TextGui.java
+++ src/main/java/com/Noah4320/NoahCraft/client/render/screen/TextGui.java
@@ -1,5 +1,6 @@
 package com.Noah4320.NoahCraft.client.render.screen;
 
+import com.Noah4320.NoahCraft.NoahCraft;
 import com.Noah4320.NoahCraft.client.event.ClientEvents;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
@@ -18,12 +19,8 @@ public class TextGui extends Screen{
 
 	private TextFieldWidget versionTextField;
 	private Button confirmBtn;
-	private final int WIDTH = this.width;
-	private final int HEIGHT = this.height;
-	//private ResourceLocation guiImage = new ResourceLocation("textures/gui/basic_gui.png");
+	private ResourceLocation guiImage = new ResourceLocation(NoahCraft.MOD_ID, "textures/gui/basic_gui.png");
 	private Minecraft mc = Minecraft.getInstance();
-	//private int xSize = 256;
-	//private int ySize = 256;
 	
 	public TextGui() {
 		super(new TranslationTextComponent("screen.guipunishkick.spawn"));
@@ -34,8 +31,8 @@ public class TextGui extends Screen{
 	protected void init() {
 		super.init();
 		FontRenderer renderer = mc.fontRenderer;
-		this.versionTextField = new TextFieldWidget(renderer, WIDTH / 2 + 150, HEIGHT / 2 + 100, 150, 20, null);
-		this.confirmBtn = new Button(WIDTH / 2 + 175, HEIGHT / 2 + 150, 80, 20, new StringTextComponent("Confirm"), new IPressable() {
+		versionTextField = new TextFieldWidget(renderer, 150, 100, 150, 20, new StringTextComponent("Test"));
+		confirmBtn = new Button(180, 145, 80, 20, new StringTextComponent("Confirm"), new IPressable() {
 			
 			@Override
 			public void onPress(Button p_onPress_1_) {
@@ -43,24 +40,19 @@ public class TextGui extends Screen{
 				mc.currentScreen.closeScreen();	
 			}
 		});
-		addButton(this.confirmBtn);
+		addButton(confirmBtn);
+		addButton(versionTextField);
 		versionTextField.setText(ClientEvents.versionText);
-		versionTextField.setFocused2(true);
-		versionTextField.setVisible(true);
 	}
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
-		//RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-		this.setListener(this.versionTextField);
-		this.versionTextField.render(matrixStack, mouseX, mouseY, partialTicks);
-		this.confirmBtn.render(matrixStack, mouseX, mouseY, partialTicks);
-		//renderBackground(matrixStack);
-		//mc.getTextureManager().bindTexture(guiImage);
-		 //int edgeSpacingX = (this.width - this.xSize) / 2;
-		    //int edgeSpacingY = (this.height - this.ySize) / 2;
-		    //this.blit(matrixStack, edgeSpacingX, edgeSpacingY, 0, 0, this.xSize, this.ySize);
+
+		versionTextField.render(matrixStack, mouseX, mouseY, partialTicks);
+		confirmBtn.render(matrixStack, mouseX, mouseY, partialTicks);
+		renderBackground(matrixStack);
+		mc.getTextureManager().bindTexture(guiImage);
+		this.blit(matrixStack, 137, 90, 0, 0, 256, 256);
 		super.render(matrixStack, mouseX, mouseY, partialTicks);
 	}
 	
